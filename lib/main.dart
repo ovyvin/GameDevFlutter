0a7fb7f31305ed86893bec357dae93f72e326437
Ovy_Vintila <ovyvin@gmail.com>
2026-04-01 07:28:55 +0000

    20260401_Flutter_5

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,8 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:gamedevflutter/main/projectfile.dart';
+import 'package:gamedevflutter/main/pixel_adventure.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  PixelAdventure game = new PixelAdventure();
+  runApp(GameWidget(game: game));
 }
