4997f493e6e6d0cfbda05013fa97994d63e29b5c
Ovy_Vintila <ovyvin@gmail.com>
2026-04-02 07:12:31 +0000

    20260402_FlutterGame_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:gamedevflutter/main/pixel_adventure.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+
   PixelAdventure game = new PixelAdventure();
   runApp(GameWidget(game: game));
 }
