6098e3280623cae011a0808981b2841442752103
Ovy_Vintila <ovyvin@gmail.com>
2026-04-03 06:57:26 +0000

    20260403_flutter_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gamedevflutter/main/pixel_adventure.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.fullScreen();
+  Flame.device.fullScreen(); //game will run in fullscreen
+  Flame.device.setLandscape(); //game will run in landscape
 
   PixelAdventure game = new PixelAdventure();
-  runApp(GameWidget(game: game));
+  runApp(GameWidget(game: kDebugMode ? PixelAdventure() : game));
 }
